f4825ec76b9c29c0ca7759881b62395b352760c4
kanekoshinya <knksny777@icloud.com>
2020-08-22 04:51:58 +0900

    日付操作クラス課題再提出

--- Javastudy/2-6/src/study/Study.java
+++ Javastudy/2-6/src/study/Study.java
@@ -35,21 +35,23 @@ public class Study {
         // 問③ 上記の「calendar」を使用し、本日から1年2ヶ月15日先の日付を表示しなさい。
         // - この課題に関しては課題実施日によって結果が異なりますので、Wiki課題の画像とは結果が異なります。
         // - また、うるう年の場合は設定内容が正しくとも日付がズレて表示される可能性があります。
-        System.out.println(calendar.get(Calendar.YEAR)+ 1);
-        System.out.println(calendar.get(Calendar.MONTH) + 3);
-        System.out.println(calendar.get(Calendar.DATE)+15);
-
-
+        calendar.add(Calendar.YEAR,1);
+        calendar.add(Calendar.MONTH,2);
+        calendar.add(Calendar.DATE,15);
+        System.out.println(calendar.get(Calendar.YEAR));
+        System.out.println(calendar.get(Calendar.MONTH) + 1);
+        System.out.println(calendar.get(Calendar.DATE));      
 
         // うるう年判定
-        printLeapYear(calendar.get(Calendar.YEAR)+1);
+        printLeapYear(calendar.get(Calendar.YEAR));
     }
-
+    
     /**
      * うるう年判定
      * 問③で日付がズレる可能性があるため補足として出力
      * @param year
      */
+
     private static void printLeapYear(final int year) {
         System.out.println(year + "年は、うるう年" + (Year.isLeap(year) ? "です。" : "ではありません。"));
     }
