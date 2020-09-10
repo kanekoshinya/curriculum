686fc7edda89205c4ca532bb660a6bd3ae02f595
kanekoshinya <knksny777@icloud.com>
2020-09-10 15:39:56 +0900

    3-4課題

--- Javastudy/2-3/src/study/Task.java
+++ Javastudy/2-3/src/study/Task.java
@@ -9,8 +9,8 @@ public class Task extends Calculator{
     public void doTask() {
         // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
         // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
-    	String a = "pulsメソッドの因数が1つの場合："+ super.plus(10);
-    	System.out.println(a);
+//    	String a = "pulsメソッドの因数が1つの場合："+ super.plus(10);
+    	System.out.println("pulsメソッドの因数が1つの場合："+ super.plus(10));
     	String b = "pulsメソッドの因数が2つの場合："+ super.plus(10,20);
     	System.out.println(b);
     	String c = "pulsメソッドの因数が3つの場合："+ super.plus(10,20,30);
