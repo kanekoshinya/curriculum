65372eb3f869c6cb120d7e86559ceb1be42941d8
kanekoshinya <knksny777@icloud.com>
2021-01-12 07:19:19 +0900

    schootest

---
+++ Javastudy/schoo-3/src/main/java/schoo1/Schoo3Application.java
@@ -0,0 +1,13 @@
+package schoo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Schoo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Schoo3Application.class, args);
+	}
+
+}
---
+++ Javastudy/schoo-3/src/test/java/schoo1/Schoo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package schoo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Schoo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
