a77c77fc6eb1fbd75301ff28a2df61bcaa941a4c
kanekoshinya <knksny777@icloud.com>
2021-03-07 20:57:26 +0900

    Springtest

--- Javastudy/attendance-5/src/main/java/attendance/SequrityConfiguration.java
+++ Javastudy/attendance-5/src/main/java/attendance/SequrityConfiguration.java
@@ -1,5 +1,51 @@
 package attendance;
 
-public class SequrityConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-}
+  @Configuration
+  public class SequrityConfiguration extends WebSecurityConfigurerAdapter {
+//	  @Override
+//	  protected void configure(HttpSecurity http) throws Exception {
+//	    http.authorizeRequests().antMatchers("/") // /homeに一致するリクエストは
+//	                            .permitAll()          // 誰でもアクセス可能
+//	                            .anyRequest()         // その他のリクエストは
+//	                            .authenticated()      // 認証が必要
+//	         .and().formLogin()
+//	         .and().httpBasic();
+//	  }
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        //ログインページを指定。
+	        //ログインページへのアクセスは全員許可する。
+	        http.formLogin()
+	            .loginPage("/login")
+	            .permitAll();
+
+	        http.authorizeRequests()
+	            .anyRequest().authenticated();
+	    }
+	  @Override
+	  public void configure(WebSecurity web) throws Exception {
+	      web.ignoring().antMatchers("/css/**");
+	  }
+
+	    @Bean
+	    PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
+//	  @Override
+//	  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//	    auth.inMemoryAuthentication()
+//	        .withUser("user").password("password").roles("USER")
+//	        .and()
+//	        .withUser("admin").password("adminpassword").roles("ADMIN");
+//	  }
+	}
+
+
--- Javastudy/attendance-5/src/main/java/attendance/controller/AttendanceController.java
+++ Javastudy/attendance-5/src/main/java/attendance/controller/AttendanceController.java
@@ -1,12 +1,28 @@
 package attendance.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class AttendanceController {
+//	@GetMapping("/login")
+// public String index() {
+//	 return "top";
+// }
+//}
+//
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AttendanceController {
-	@GetMapping("/")
- public String index() {
-	 return "top";
- }
+
+    @RequestMapping("/login")
+    public String showLoginForm(Model model) {
+
+        //ログイン画面へ遷移。
+        return "top";
+    }
 }
