a6ec10830d6c3315097d379ce703009c2670dc68
kanekoshinya <knksny777@icloud.com>
2021-04-08 01:36:32 +0900

    1-3

---
+++ JavaScriptStudy/1-3/main.js
@@ -0,0 +1,25 @@
+let a = 11;
+let b = 16;
+let x = 10;
+let y = 0;
+
+if(a!==10){
+  console.log(a);
+}
+
+if(10 <= b <= 20 && b%2===0){
+  console.log(b);
+}
+
+if(10<=x<=20){
+  console.log("成功です");
+}else{
+  console.log("失敗です");
+}
+
+if(y%2==0){
+  console.log("偶数です");
+}else{
+  console.log("奇数です");
+}
+
